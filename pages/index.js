69a0211024cccfc0f9c33b2ab2db45498ff9749f
Batudo <batudo@2pi.finance>
2021-06-27 16:35:27 -0300

    Actual email saving

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,64 @@
 import Head from 'next/head'
+import { useState } from 'react'
+
+const url = process.env.NEXT_PUBLIC_POST_URL
 
 export default function Home() {
+  const [email, setEmail]               = useState('')
+  const [enabled, setEnabled]           = useState(false)
+  const [inputEnabled, setInputEnabled] = useState(true)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [alertStyle, setAlertStyle]     = useState('success')
+
+  const onChange = e => {
+    const value = e.target.value
+
+    setEmail(value)
+    setEnabled(/\S+@\S+\.\S+/.test(value) && value.length < 255)
+  }
+
+  const handleClick = event => {
+    event.preventDefault()
+
+    setEnabled(false)
+    setInputEnabled(false)
+
+    fetch(url, {
+      method:  'POST',
+      body:    JSON.stringify({ email }),
+      headers: {
+        'Accept':       'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(
+      response => response.json()
+    ).then(() => {
+      setAlertMessage("Thanks, we'll be in touch!")
+      setAlertStyle('success')
+    }).catch(error => {
+      setEnabled(true)
+      setAlertMessage('Ups, something went wrong, please try again!')
+      setAlertStyle('danger')
+    }).finally(() => {
+      setEmail('')
+      setInputEnabled(true)
+    })
+  }
+
+  const renderAlert = () => {
+    if (alertMessage) {
+      return (
+        <div className={`alert alert-${alertStyle}`}>
+          <p className="text-center mb-0">
+            {alertMessage}
+          </p>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
+
   return (
     <div className="container">
       <Head>
@@ -53,15 +111,36 @@ export default function Home() {
 
       <div className="row">
         <div className="col-12 col-lg-6">
-          <div className="input-group mb-3">
-            <span className="input-group-text bg-white border-end-0 pe-1">
-              <i className="bi-envelope"></i>
-            </span>
-            <input type="email" className="form-control border-start-0 ps-2" aria-label="Email" placeholder="Email address" />
-            <button className="btn btn-info" type="button">Join Waitlist</button>
-          </div>
+          <form>
+            <div className="input-group mb-3">
+              <span className="input-group-text bg-white border-end-0 pe-1">
+                <i className="bi-envelope"></i>
+              </span>
+
+              <input type="email"
+                     name="emailAddress"
+                     className="form-control border-start-0 ps-2"
+                     autoComplete="email"
+                     maxLength="254"
+                     aria-label="Email"
+                     placeholder="Email address"
+                     value={email}
+                     disabled={! inputEnabled}
+                     onKeyDown={e => onChange(e) && e.preventDefault()}
+                     onChange={onChange} />
+
+              <button className="btn btn-info"
+                      type="submit"
+                      disabled={! enabled}
+                      onClick={handleClick}>
+                Join Waitlist
+              </button>
+            </div>
+          </form>
         </div>
       </div>
+
+      {renderAlert()}
     </div>
   )
 }
